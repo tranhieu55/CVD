8c9d6a208dc7d2243e3d97928c953105e39ca62c
Blake <blake@convertdigital.com.au>
2020-05-29 17:55:13 +1000

    started front end work

--- src/components/slices/BigText.js
+++ src/components/slices/BigText.js
@@ -3,15 +3,11 @@ import styled from "styled-components"
 import PropTypes from "prop-types"
 
 const BigText = ({ input }) => {
-  console.log("input", input)
-
   const heading = input.primary.text[0].text
 
   return (
     <>
-      <div>
-        <SubHeading>{heading}</SubHeading>
-      </div>
+      <SubHeading>{heading}</SubHeading>
     </>
   )
 }
@@ -24,4 +20,9 @@ BigText.propTypes = {
 
 const SubHeading = styled.h2`
   text-align: center;
+  font-size: 3rem;
+  width: 75%;
+  margin: auto;
+  height: auto;
+  padding: 150px 0;
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,38 @@ import React from "react"
 import SliceZone from "../utils/SliceZone"
 import styled from "styled-components"
 import SEO from "../components/utilities/SEO"
+import Layout from "../components/Layout"
+import { Link } from "gatsby"
 
 const Index = ({ data: { prismic } }) => {
   const data = prismic.allHomepages.edges[0].node
-  const title = prismic.allHomepages.edges[0].node.page_title[0].text
+  const title = data.page_title[0].text
+  const featured_image = data.featured_image.url
   console.log("index", data)
   return (
-    <>
+    <Layout>
       <SEO props={data} />
-      <Title>{title}</Title>
+      <Container>
+        <ContainerLeft>
+          <Title>{title}</Title>
+          <ButtonContainer>
+            <Button yellow>
+              <ButtonText yellow to="">
+                Get in touch
+              </ButtonText>
+            </Button>
+            <Button>
+              <ButtonText to="">View video</ButtonText>
+            </Button>
+          </ButtonContainer>
+        </ContainerLeft>
+        <ContainerRight>
+          <IPhone src={featured_image} />
+        </ContainerRight>
+      </Container>
+      {/* slize zone below */}
       <SliceZone allSlices={data.body} />
-    </>
+    </Layout>
   )
 }
 
@@ -20,7 +41,74 @@ export default Index
 
 //styles
 
-const Title = styled.h1``
+const Title = styled.h1`
+  font-size: 6rem;
+  color: white;
+`
+
+const Container = styled.div`
+  display: grid;
+  height: 75vh;
+  width: 100%;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 100%;
+  margin: auto;
+  background-color: black;
+  /* :after {
+    background: inherit;
+    bottom: 0;
+    content: "";
+    display: block;
+    height: 50%;
+    left: 0;
+    position: absolute;
+    right: 0;
+    transform: skewY(-2.5deg);
+    transform-origin: 100%;
+    z-index: -1;
+  } */
+`
+const ContainerLeft = styled.div`
+  grid-column: 1;
+  width: 75%;
+  margin-left: auto;
+  margin-top: auto;
+  margin-bottom: 50px;
+`
+
+const Button = styled.div`
+  background: ${props => (props.yellow ? "#FECF09" : "black")};
+  color: ${props => (props.yellow ? "#000000" : "black")};
+  padding: 15px 30px;
+  border-radius: 4px;
+  border: solid 1px;
+  border-color: ${props => (props.yellow ? "#FECF09" : "white")};
+  position: relative;
+  margin-right: 10px;
+`
+
+const ButtonText = styled(Link)`
+  color: ${props => (props.yellow ? "#000000" : "white")};
+  position: relative;
+  font-weight: 600;
+  top: 2px;
+`
+
+const ButtonContainer = styled.div`
+  display: inline-flex;
+`
+
+const ContainerRight = styled.div`
+  grid-column: 2;
+  width: 75%;
+  margin: auto;
+  margin-left: 20%;
+`
+const IPhone = styled.img`
+  height: 80%;
+  top: 75px;
+  position: relative;
+`
 
 export const pageQuery = graphql`
   query IndexQuery {
@@ -29,7 +117,7 @@ export const pageQuery = graphql`
         edges {
           node {
             page_title
-            featured_iamge
+            featured_image
             #seo
             meta_title
             meta_description
