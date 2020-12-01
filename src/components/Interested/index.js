8bc37dfa1dfb34649dd86e37738d36f805b7ae8b
tuanva <tuanva@mirdetail.com>
2020-12-01 20:21:16 +0700

    change

--- src/components/Interested/index.js
+++ src/components/Interested/index.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react"
-import { Form, Modal } from "react-bootstrap"
+import React from "react"
 import styled from "styled-components"
 import { theme } from "../../utils/theme"
 import ButtonCustom from "../ButtonCustom"
@@ -7,30 +6,30 @@ import H2 from "../H2"
 import P from "../Typography"
 
 const InterestedStyle = styled.div`
-  clip-path: polygon(0 15%,100% 0,100% 100%,0% 100%);
+  clip-path: polygon(0 15%, 100% 0, 100% 100%, 0% 100%);
   background-color: #101010;
   width: 100%;
   color: white;
   margin: -2px auto;
-  .button-header{
-    .get-in-touch{
+  .button-header {
+    .get-in-touch {
       height: 20px;
-    width: 99px;
-    color: #FFFFFF;
-    font-family: 'Calibre Semibold';
-    font-size: 20px;
-    font-weight: 600;
-    letter-spacing: 0;
-    line-height: 24px;
-    text-align: center;
-  }
+      width: 99px;
+      color: #ffffff;
+      font-family: "Calibre Semibold";
+      font-size: 20px;
+      font-weight: 600;
+      letter-spacing: 0;
+      line-height: 24px;
+      text-align: center;
+    }
   }
   h2 {
     font-size: 64px;
     font-weight: bold;
-    margin-bottom:0px;
-    max-width:500px;
-    margin:auto;
+    margin-bottom: 0px;
+    max-width: 500px;
+    margin: auto;
   }
   p {
     /* font-size: 24px; */
@@ -40,9 +39,9 @@ const InterestedStyle = styled.div`
   .boxInterested {
     padding-top: 162px;
     padding-bottom: 108px;
-    max-width:700px;
-    margin:auto;
-    text-align:center;
+    max-width: 700px;
+    margin: auto;
+    text-align: center;
   }
   .box-btn {
     position: relative;
@@ -51,8 +50,8 @@ const InterestedStyle = styled.div`
     transform: translate(270px);
     opacity: 0.5;
     overflow: hidden;
-}
-  .my-btn::before{
+  }
+  .my-btn::before {
     background-color: white;
     content: "";
     height: 155px;
@@ -66,16 +65,14 @@ const InterestedStyle = styled.div`
     width: 50px;
     z-index: 1;
   }
-  
 
-  
   /* Extra small devices (phones, 600px and down) */
   @media only screen and (max-width: 600px) {
     clip-path: polygon(0 8%, 100% 0%, 100% 100%, 0% 100%);
     h2 {
       font-size: 32px;
-      max-width:250px;
-      margin:auto;
+      max-width: 250px;
+      margin: auto;
       line-height: 33px;
     }
     p {
@@ -88,15 +85,14 @@ const InterestedStyle = styled.div`
       padding-top: 73px;
       padding-bottom: 48px;
     }
-   
   }
 
   /* Small devices (portrait tablets and large phones, 600px and up) */
   @media only screen and (min-width: 600px) {
     h2 {
       font-size: 40px;
-      max-width:320px;
-      margin:auto;
+      max-width: 320px;
+      margin: auto;
     }
     p {
       font-size: 20px;
@@ -107,11 +103,11 @@ const InterestedStyle = styled.div`
   @media only screen and (min-width: 768px) {
     h2 {
       font-size: 40px;
-      max-width:325px;
-      margin:auto;
+      max-width: 325px;
+      margin: auto;
     }
     p {
-      margin-top:20px;
+      margin-top: 20px;
       font-size: 24px;
     }
   }
@@ -120,8 +116,8 @@ const InterestedStyle = styled.div`
   @media only screen and (min-width: 992px) {
     h2 {
       font-size: 64px;
-      max-width:500px;
-      margin:auto;
+      max-width: 500px;
+      margin: auto;
     }
     p {
       font-size: 24px;
@@ -132,122 +128,56 @@ const InterestedStyle = styled.div`
   @media only screen and (min-width: 1200px) {
     h2 {
       font-size: 64px;
-      max-width:500px;
-      margin:auto;
-      font-family: 'Calibre Bold';
-      color: #FFFFFF;
+      max-width: 500px;
+      margin: auto;
+      font-family: "Calibre Bold";
+      color: #ffffff;
       font-weight: bold;
       line-height: 54px;
     }
     p {
       font-size: 24px;
-      margin-top:6px;
-      color: #FFFFFF;
+      margin-top: 6px;
+      color: #ffffff;
       margin-bottom: 28px;
     }
     .boxInterested {
-    padding-top: 173px;
-    padding-bottom: 108px;
-  }
-  }
-
-
-`
-
-const BoxModal = styled.div`
-  h2 {
-    font-weight: bold;
-  }
-  .btn-w {
-    color: gold;
+      padding-top: 173px;
+      padding-bottom: 108px;
+    }
   }
 `
 
-export default function Interested(props) {
-  const [show, setShow] = useState(false)
-
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+export default function Interested() {
   return (
     <InterestedStyle>
-      <div className='boxInterested'>
-        <H2 lett='-1' fz="64" lineh='54' fontFamily="Calibre Bold">
+      <div className="boxInterested">
+        <H2 lett="-1" fz="64" lineh="54" fontFamily="Calibre Bold">
           Interested in working with us?
-      </H2>
-        <P lineh='30' fontFamily="Calibre Regular" mrb="30">Send us a message and we'll get back to you as soon as possible</P>
-        <div className=''>
-          <ButtonCustom className='my-btn-back my-btn button-header'
+        </H2>
+        <P lineh="30" fontFamily="Calibre Regular" mrb="30">
+          Send us a message and we'll get back to you as soon as possible
+        </P>
+        <div className="">
+          <ButtonCustom
+            className="my-btn-back my-btn button-header"
             bgColor={theme.colors.black}
             textColor={theme.colors.white}
             variant="primary"
-            onClick={handleShow}
-            fz='20'
-            fw='600'
-            wt='166'
-            lineh='24'
-            ht='56'
+            fz="20"
+            fw="600"
+            pd1="15"
+            pd2="30.5"
+            lineh="24"
             wspace="nowrap"
-
           >
             <span></span>
             <span></span>
             <span></span>
             <span></span>
-            <h2 className='get-in-touch'>Get in touch</h2>
+            <h2 className="get-in-touch">Get in touch</h2>
           </ButtonCustom>
         </div>
-        <BoxModal>
-          <Modal show={show} onHide={handleClose} animation={false}>
-            <Modal.Header closeButton>
-              <Modal.Title>
-                <H2>Get in touch</H2>
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Form.Group>
-                <Form.Control size="lg" type="text" placeholder="Name*" />
-                <br />
-                <Form.Control size="lg" type="email" placeholder="Email*" />
-                <br />
-                <Form.Control size="lg" type="text" placeholder="Large text" />
-                <br />
-                <Form.Control size="lg" type="text" placeholder="Company" />
-                <br />
-                <Form.Control
-                  size="lg"
-                  type="text"
-                  placeholder="Current Website URL"
-                />
-                <br />
-                <textarea
-                  className="form-control"
-                  id="exampleFormControlTextarea1"
-                  rows="4"
-                  placeholder="Message/Requirement"
-                />
-                <br />
-              </Form.Group>
-              <ButtonCustom
-                className='my-btn-back my-btn button-header'
-                bgColor={theme.colors.primaryColor}
-                textColor={theme.colors.black}
-                variant="primary"
-                onClick={handleShow}
-                fontFamily="Calibre Regular"
-                pd1="15"
-                pd2="30.5"
-                fz='20'
-              // lineh='56'
-              >
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <h2 className='get-in-touch'>Get in touch</h2>
-              </ButtonCustom>
-            </Modal.Body>
-          </Modal>
-        </BoxModal>
       </div>
     </InterestedStyle>
   )
