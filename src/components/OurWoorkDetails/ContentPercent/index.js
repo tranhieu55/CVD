3548636413470bd41fc5531d8ecd7cd19096c746
Hieptv <hieptv1998@gmail.com>
2021-02-17 18:07:29 +0700

    fix feedback

--- src/components/OurWoorkDetails/ContentPercent/index.js
+++ src/components/OurWoorkDetails/ContentPercent/index.js
@@ -6,42 +6,66 @@ export default function ContentPercent(props) {
     <>
       <Percent className="plus">
         {props.input.fields.map((item, index) => (
-          <div key={index}>
-            <h1>{item.number_of_statistical[0].text}</h1>
-            <h5>{item.description_of_statistical[0].text}</h5>
-          </div>
+          <Content>
+            <TiTle>{item.number_of_statistical[0].text}</TiTle>
+            <TextPercent>{item.description_of_statistical[0].text}</TextPercent>
+          </Content>
         ))}
       </Percent>
     </>
   )
 }
+const TiTle = styled.h1`
+  font-weight: bold;
+  font-size: 64px;
+  text-align: center;
+  font-family: "Calibre Semibold";
+  line-height: 56px;
+  color: #101010;
+  font-weight: 600;
+  margin-bottom: 0px;
+  @media only screen and (max-width: 600px){
+    font-size: 48px;
+    color: #101010;
+    line-height: 36px;
+  }
+`
+const TextPercent = styled.h5`
+  text-align: center;
+  font-size: 18px;
+  height: 26px;
+  font-family: "Calibre Regular";
+  color: #222222;
+  line-height: 20px;
+  @media only screen and (max-width: 600px){
+    line-height: 32px;
+    margin-bottom: 0px;
+  }
+`
+const Content = styled.div`
+   width: 26%;
+      @media only screen and (max-width: 600px) {
+        margin-left: 0px !important;
+        width: 100%;
+        height: 80px;
+        margin-bottom: 32px;
+      }
+      @media (min-width: 1024px){
+        width: 100%;
+      }
+      @media (min-width: 1200px){
+        width: 30%;
+      }
+      @media (min-width: 1600px){
+        width: 270px;
+      }
+`
 const Percent = styled.div`
     display: flex;
     margin-left: 128px;
     justify-content: space-between;
     margin-right: 128px;
     height: 86px;
-    div {
-      width: 26%;
-      h1 {
-        font-weight: bold;
-        font-size: 64px;
-        text-align: center;
-        font-family: "Calibre Semibold";
-        line-height: 56px;
-        color: #101010;
-        font-weight: 600;
-        margin-bottom: 0px;
-      }
-      h5 {
-        text-align: center;
-        font-size: 18px;
-        height: 26px;
-        font-family: "Calibre Regular";
-        color: #222222;
-        line-height: 20px;
-      }
-    }
     @media only screen and (max-width: 600px) {
       margin-bottom: 24px;
       display: block !important;
@@ -49,21 +73,6 @@ const Percent = styled.div`
       margin-left: 37px;
       margin-right: 36px;
       height: 304px;
-      div {
-        margin-left: 0px !important;
-        width: 100%;
-        height: 80px;
-        margin-bottom: 32px;
-        h1 {
-          font-size: 48px;
-          color: #101010;
-          line-height: 36px;
-        }
-        h5 {
-          line-height: 32px;
-          margin-bottom: 0px;
-        }
-      }
     }
     @media (min-width: 601px) {
       margin-left: 0px;
@@ -76,11 +85,6 @@ const Percent = styled.div`
   @media (min-width: 1024px) {
     margin-bottom: 50px;
     max-width: 900px;
-    .plus {
-      div {
-        width: 100%;
-      }
-    }
     .text-aliquam {
       padding-top: 50px;
     }
@@ -88,16 +92,10 @@ const Percent = styled.div`
   @media (min-width: 1200px) {
       display: flex;
       margin-bottom: 96px;
-      div {
-        width: 30%;
-      }
   }
   @media (min-width: 1600px) {
         max-width: 986px;
         margin: 0 auto 86px;
         display: flex;
-        div {
-            width: 270px;
-        }
     }
 `
