e3b7f65bd59e5365b6a1724625d456dc11025bf9
Hoanvv <hoanvv@arrowhitech.com>
2021-04-22 10:46:03 +0700

    update style guide

--- src/context/ourwork/OurWorkContextProvider.js
+++ src/context/ourwork/OurWorkContextProvider.js
@@ -10,7 +10,7 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "ADD_FILTER_ITEM":
-      console.log("action - reducer : ", { action })
+      // console.log("action - reducer : ", { action })
       return {
         ...state,
         listSelected: action.value,
