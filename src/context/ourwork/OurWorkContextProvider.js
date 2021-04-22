cc9df344914b41e95c6584423faeb4098def0d12
Hoanvv <hoanvv@arrowhitech.com>
2021-04-22 10:24:41 +0700

    test deloy netlify

--- src/context/ourwork/OurWorkContextProvider.js
+++ src/context/ourwork/OurWorkContextProvider.js
@@ -10,7 +10,8 @@ const initialState = {
 function reducer(state, action) {
   switch (action.type) {
     case "ADD_FILTER_ITEM":
-      console.log({ action })
+      console.log("action : ", { action })
+
       return {
         ...state,
         listSelected: action.value,
