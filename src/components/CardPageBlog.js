91fe72fb5dc324ea804d658e89a4ed4adabef443
Hoanvv <hoanvv@arrowhitech.com>
2021-04-27 14:57:13 +0700

    update code

--- src/components/CardPageBlog.js
+++ src/components/CardPageBlog.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import moment from "moment"
 import React from "react"
 import styled from "styled-components"
@@ -11,12 +12,14 @@ const OurLaster = ({ input }) => {
   return (
     <OurLasters>
       {input.fields.map((item, index) => (
-        <ListPost key={index}>
-          <Img
-            src={item.post_item.post_image.url}
-            vitri={index}
-            al={item.post_item.post_image?.alt}
-          ></Img>
+        <ListPost key={index} className={index % 2 === 0 ? "orderleft" : ""}>
+          <Link to={`/blog/${item.post_item._meta.uid}`}>
+            <Img
+              src={item.post_item.post_image.url}
+              vitri={index}
+              al={item.post_item.post_image?.alt}
+            ></Img>
+          </Link>
           <SubTitle vitri={index}>
             {moment(item.post_item?.date_created).format("LL")}
           </SubTitle>
@@ -30,53 +33,46 @@ const OurLaster = ({ input }) => {
 export default OurLaster
 
 const OurLasters = styled.div`
-  height: 716px;
-  position: relative;
-  margin: 96px 184px 0px;
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 1240px;
+  margin: auto;
+  margin-top: 64px;
+  .orderleft {
+    margin-right: 28px;
+  }
   @media (max-width: 600px) {
-    height: 862px;
-    margin: 32px 16px 37px;
-  }
-  @media (max-width: 360px) {
-    margin: 32px 16px 57px;
-  }
-  @media (min-width: 600px) {
-    height: 1243px;
-    margin: 50px 16px 37px;
-  }
-  @media (min-width: 1024px) {
-    height: 716px;
-    margin: 50px 16px 37px;
-  }
-  @media (min-width: 1600px) {
-    max-width: 1240px;
-    margin: auto;
+    padding: 16px;
+    .orderleft {
+      margin-right: 0px;
+    }
   }
 `
 
 const ListPost = styled.div`
   height: 517px;
   width: 606px;
+  margin-bottom: 84px;
+
   @media (max-width: 600px) {
     height: 351px;
     width: 100%;
-    margin-bottom: 20px;
+    margin-bottom: 50px;
   }
-  @media (min-width: 768px) {
+  /* @media (min-width: 768px) {
     width: 100%;
     margin-bottom: 16px;
   }
   @media (min-width: 1024px) {
     width: 49%;
     height: 517px;
-  }
+  } */
 `
 
 const Img = styled.img`
-  width: ${props => (props.vitri === 1 ? "80%" : "100%")};
-  height: ${props => (props.vitri === 1 ? "360px" : "400px")};
+  height: 400px;
   margin-left: 32px;
-  margin-top: ${props => (props.vitri === 1 ? "20px" : "0px")};
+  margin-top: 0px;
   object-fit: cover;
   @media (max-width: 600px) {
     width: 100%;
@@ -88,13 +84,13 @@ const Img = styled.img`
     margin-left: 0px;
   }
   @media (min-width: 1024px) {
-    width: ${props => (props.vitri === 1 ? "80%" : "100%")};
-    height: ${props => (props.vitri === 1 ? "360px" : "400px")};
+    width: 100%;
+    height: 400px;
     margin-left: 32px;
-    margin-top: ${props => (props.vitri === 1 ? "20px" : "0px")};
+    margin-top: 0px;
   }
   @media (min-width: 1366px) {
-    width: ${props => (props.vitri === 1 ? "80%" : "95%")};
+    width: 95%;
   }
 `
 const TitlePost = styled.h4`
@@ -106,16 +102,16 @@ const TitlePost = styled.h4`
   letter-spacing: -0.5px;
   line-height: 32px;
   margin-left: 80px;
-  @media (max-width: 600px) {
-    height: 72px;
-    font-size: 24px;
-    font-weight: 600;
-    letter-spacing: -0.25px;
-    line-height: 26px;
-    margin-left: 48px;
-  }
   @media (max-width: 320px) {
     width: 249px;
+  }
+  @media (max-width: 600px) {
+    height: 72px;
+    font-size: 32px;
+    font-weight: 600;
+    letter-spacing: -0.33px;
+    line-height: 26px;
+    margin-left: 48px;
   }
 `
 const Content = styled.div`
@@ -144,7 +140,7 @@ const SubTitle = styled.h4`
   height: 16px;
   width: 154px;
   color: #999999;
-  font-family: Calibre Semibold;
+  font-family: "Calibre Semibold";
   font-size: 14px;
   font-weight: 600;
   letter-spacing: 1px;
@@ -163,7 +159,7 @@ const SubTitle = styled.h4`
   }
   @media (max-width: 600px) {
     height: 14px;
-    font-family: Calibre;
+    font-family: "Calibre Semibold";
     font-size: 12px;
     font-weight: 600;
     letter-spacing: 1px;
@@ -180,6 +176,6 @@ const SubTitle = styled.h4`
   }
   @media (min-width: 1366px) {
     margin-left: 80px;
-    margin-top: ${props => (props.vitri === 1 ? "53px" : "33px")};
+    margin-top: 33px;
   }
 `
