d7e655974665de2c54d67599a7c5c78aad6032da
Hiep Trinh Van <hieptv@arrowhitech.net>
2021-04-27 10:58:24 +0700

    update google map

--- src/components/Contact/Derection/index.js
+++ src/components/Contact/Derection/index.js
@@ -81,12 +81,18 @@ export default function Derection ({input}) {
         }
         @media(max-width: 600px){
             width: 100%;
+            margin-top: 24px;
+            height: 219px;
         }
         @media(min-width: 600px){
             width: 100%;
+            margin-top: 24px;
+            height: 219px;
         }
         @media(min-width: 1024px){
             width: 48.9%;
+            margin-top: 0px;
+            height: 100%;
         }
     `
     const Content = styled.div`
@@ -97,12 +103,15 @@ export default function Derection ({input}) {
         margin-bottom: 40px;
         @media(max-width: 600px){
             display: block;
+            height: auto;
         }
         @media(min-width: 600px){
             display: block;
+            height: auto;
         }
         @media(min-width: 1024px){
             display: flex;
+            height: 172px;
         }
     `
     const TitleLeft = styled.div`
@@ -157,7 +166,7 @@ export default function Derection ({input}) {
                             </DivLink>
                         </Contentleft>
                     <ContentRight>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.2612906442637!2d144.9868783163482!3d-37.80734834145114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642d8a7795b53%3A0x7efe11a86e7e0b15!2sConvert%20Digital!5e0!3m2!1sen!2sau!4v1619490398964!5m2!1sen!2sau" 
+                    <iframe src={item.google_maps_link.url} 
                         width="100%" height="100%" className='googole-map' allowfullscreen="" loading="lazy"
                     >
                     </iframe>
