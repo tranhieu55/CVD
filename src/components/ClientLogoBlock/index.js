c47417ded318a7f243dcf9aef5ab80881e34d863
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-05-21 09:40:21 +0700

    responsive done

--- src/components/ClientLogoBlock/index.js
+++ src/components/ClientLogoBlock/index.js
@@ -83,30 +83,27 @@ const Logoes = styled.div`
   @media (max-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-  @media (max-width: 600px) {
-    grid-template-columns: 1fr;
-    grid-gap: 30px;
-    padding-bottom: 30px;
+  @media (max-width: 768px) {
+    grid-column-gap: 17px;
+    grid-row-gap: 14.5px;
   }
 `
 
 const BlockLogo = styled.div`
-  //   width: 100%;
-  //   max-width: 246px;
   width: 246px;
 
-  @media (max-width: 992px) {
-  }
-
   @media (max-width: 600px) {
+    width: 163px;
+  }
+  @media (max-width: 370px) {
+    width: 44vw;
   }
 `
 
 const IMG = styled.div`
   width: 100%;
   height: 122px;
-  margin-bottom: 20px;
-  background-color: gray;
+  background-color: #e8e8e8;
 
   img {
     width: 100%;
@@ -115,5 +112,6 @@ const IMG = styled.div`
   }
 
   @media (max-width: 600px) {
+    height: 80px;
   }
 `
