0c47097d79b8c6226d86a099b91a1bc0cfce46ff
hieutt <hieu050599@gmail.com>
2021-05-26 17:20:05 +0700

    update code menu

--- src/components/NewLetter/index.js
+++ src/components/NewLetter/index.js
@@ -1,74 +1,120 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react"
+import styled from "styled-components"
 
-export default function NewLetter ({dataFooter}) {
-    const data = dataFooter ? dataFooter.edges[0]?.node.body?.filter(item => item.type === "newsletter_signup") : [];
-    const [errors, setError] = useState('')
-    const [vali, setVali] = useState('');
-    console.log(vali);
-    const validateEmail = (e) => {
-        var email = e.target.value
-    
-        if (email.length > 0) {
-            setError('')
-            setVali(email)
-        } else {
-            setError('This field is required')
-            setVali(email)
-        }
+export default function NewLetter({ dataFooter }) {
+  const data = dataFooter
+    ? dataFooter.edges[0]?.node.body?.filter(
+        item => item.type === "newsletter_signup"
+      )
+    : []
+  const [errors, setError] = useState("")
+  const [vali, setVali] = useState("")
+  console.log(vali)
+  const validateEmail = e => {
+    var email = e.target.value
+
+    if (email.length > 0) {
+      setError("")
+      setVali(email)
+    } else {
+      setError("This field is required")
+      setVali(email)
     }
-    const ValiButton = (vali) => {
-        if(vali !== ''){
-            setError('')
-        } else {
-            setError('This field is required')
-        }
+  }
+  const ValiButton = vali => {
+    if (vali !== "") {
+      setError("")
+    } else {
+      setError("This field is required")
     }
-    return(
-        <Container>
-            <Title>{data[0]?.primary?.title?.map(item => item?.text ? item?.text : item)}</Title>
-            <Text>{data[0]?.primary?.title_side?.map(item => item?.text ? item?.text : item)}</Text>
-            <DivInput>
-                <Input onChange={(e) => validateEmail(e)} type="email" placeholder={data[0]?.primary?.form_submit_placeholder?.map(item => item?.text ? item?.text : item)}></Input>
-                <Errors hienthi={errors}>{errors}</Errors>
-            </DivInput>
-            <Submit onClick={() => ValiButton(vali)}>{data[0]?.primary?.text_button?.map(item => item?.text ? item?.text : item)}</Submit>
-        </Container>
-    )
-}   
+  }
+  return (
+    <Container>
+      <Title>
+        {data[0]?.primary?.title?.map(item => (item?.text ? item?.text : item))}
+      </Title>
+      <Text>
+        {data[0]?.primary?.title_side?.map(item =>
+          item?.text ? item?.text : item
+        )}
+      </Text>
+      <DivInput>
+        <Input
+          onChange={e => validateEmail(e)}
+          type="email"
+          placeholder={data[0]?.primary?.form_submit_placeholder?.map(item =>
+            item?.text ? item?.text : item
+          )}
+        ></Input>
+        <Errors hienthi={errors}>{errors}</Errors>
+      </DivInput>
+      <Submit onClick={() => ValiButton(vali)}>
+        {data[0]?.primary?.text_button?.map(item =>
+          item?.text ? item?.text : item
+        )}
+      </Submit>
+    </Container>
+  )
+}
 const Container = styled.div`
+  display: flex;
+  @media (max-width: 600px) {
+    display: block;
+    padding: 0 16px;
+  }
+  @media (min-width: 600px) {
+    display: block;
+    padding: 0 86px;
+  }
+  @media (min-width: 768px) {
+    width: 420px;
+    margin: 0 auto;
+    padding: 0;
+  }
+  @media (min-width: 992px) {
     display: flex;
-    @media(max-width: 600px){
-        display: block;
-        padding: 0 16px;
-    }
-    @media(min-width: 600px){
-        display: block;
-        padding: 0 86px;
-    }
-    @media(min-width: 768px){
-        width: 420px;
-        margin: 0 auto;
-        padding: 0;
-    }
-    @media(min-width: 992px){
-        display: flex;
-        margin: 0px 16px;
-        border-bottom: 1px solid #92939B;
-        padding: 0 0px;
-        height: 208px;
-        width: auto;
-    }
-    @media(min-width: 1280px){
-        height: 208px;
-        width: 1240px;
-        margin: 0px auto;
-        display: flex;
-        border-bottom: 1px solid #92939B;
-    }
+    margin: 0px 16px;
+    border-bottom: 1px solid #92939b;
+    padding: 0 0px;
+    height: 208px;
+    width: auto;
+  }
+  @media (min-width: 1280px) {
+    height: 208px;
+    width: 1240px;
+    margin: 0px auto;
+    display: flex;
+    border-bottom: 1px solid #92939b;
+  }
 `
 const Title = styled.h1`
-    color: #FFFFFF;
+  color: #ffffff;
+  font-family: Calibre Bold;
+  font-size: 32px;
+  font-weight: bold;
+  letter-spacing: -0.5px;
+  line-height: 38px;
+  margin-top: 80px;
+  margin-right: 86px;
+  margin-bottom: 0px;
+  @media (max-width: 600px) {
+    padding-top: 48px;
+    text-align: center;
+    margin-top: 0px;
+    margin-right: 0px;
+  }
+  @media (min-width: 600px) {
+    padding-top: 48px;
+    text-align: center;
+    margin-top: 0px;
+    margin-right: 0px;
+  }
+  @media (min-width: 992px) {
+    padding-top: 0px;
+    margin-top: 60px;
+  }
+  @media (min-width: 1280px) {
+    color: #ffffff;
     font-family: Calibre Bold;
     font-size: 32px;
     font-weight: bold;
@@ -77,38 +123,50 @@ const Title = styled.h1`
     margin-top: 80px;
     margin-right: 86px;
     margin-bottom: 0px;
-    @media(max-width: 600px){
-        padding-top: 48px;
-        text-align: center;
-        margin-top: 0px;
-        margin-right: 0px;
-    }
-    @media(min-width: 600px){
-        padding-top: 48px;
-        text-align: center;
-        margin-top: 0px;
-        margin-right: 0px;
-    }
-    @media(min-width: 992px){
-        padding-top: 0px;
-        margin-top: 60px;
-    }
-    @media(min-width: 1280px){
-        color: #FFFFFF;
-        font-family: Calibre Bold;
-        font-size: 32px;
-        font-weight: bold;
-        letter-spacing: -0.5px;
-        line-height: 38px;
-        margin-top: 80px;
-        margin-right: 86px;
-        margin-bottom: 0px;
-    }
+  }
 `
 const Text = styled.p`
+  width: 253px;
+  opacity: 0.7;
+  color: #ffffff;
+  font-family: Calibre Medium;
+  font-size: 18px;
+  font-weight: 500;
+  letter-spacing: 0;
+  line-height: 26px;
+  margin-top: 78px;
+  margin-right: 77px;
+  margin-bottom: 0px;
+  @media (max-width: 600px) {
+    margin-top: 0px;
+    text-align: center;
+    width: auto;
+    height: auto;
+    padding: 0px 16px;
+  }
+  @media (min-width: 600px) {
+    margin-top: 0px;
+    text-align: center;
+    width: auto;
+    height: auto;
+    padding: 0px 16px;
+    margin-right: 0px;
+  }
+  @media (min-width: 992px) {
+    margin-top: 70px;
+    padding: 0px 56px;
+    text-align: left;
+  }
+  @media (min-width: 1188px) {
+    margin-top: 70px;
+    margin-right: 46px;
+    margin-left: 46px;
+    padding: 0px 16px;
+  }
+  @media (min-width: 1280px) {
     width: 253px;
     opacity: 0.7;
-    color: #FFFFFF;
+    color: #ffffff;
     font-family: Calibre Medium;
     font-size: 18px;
     font-weight: 500;
@@ -116,160 +174,163 @@ const Text = styled.p`
     line-height: 26px;
     margin-top: 78px;
     margin-right: 77px;
-    margin-bottom: 0px;
-    @media(max-width: 600px){
-        margin-top: 0px;
-        text-align: center;
-        width: auto;
-        height: auto;
-        padding: 0px 16px;
-    }
-    @media(min-width: 600px){
-        margin-top: 0px;
-        text-align: center;
-        width: auto;
-        height: auto;
-        padding: 0px 16px;
-        margin-right: 0px;
-    }
-    @media(min-width: 992px){
-        margin-top: 70px;
-        padding: 0px 56px;
-        text-align: left;
-    }
-    @media(min-width: 1188px){
-        margin-top: 70px;
-        margin-right: 46px;
-        margin-left: 46px;
-        padding: 0px 16px;
-    }
-    @media(min-width: 1280px){
-        width: 253px;
-        opacity: 0.7;
-        color: #FFFFFF;
-        font-family: Calibre Medium;
-        font-size: 18px;
-        font-weight: 500;
-        letter-spacing: 0;
-        line-height: 26px;
-        margin-top: 78px;
-        margin-right: 77px;
-        margin-left: 0px;
-        padding: 0px;
-    }
+    margin-left: 0px;
+    padding: 0px;
+  }
 `
 const Input = styled.input`
+  height: 48px;
+  width: 393px;
+  color: #bebfc6;
+  font-family: "Calibre Regular";
+  font-size: 18px;
+  letter-spacing: 0;
+  line-height: 30px;
+  margin-top: 78px;
+  border: 1px solid #92939b;
+  border-radius: 3px;
+  box-sizing: border-box;
+  background: transparent;
+  padding: 0px 15px;
+  margin-right: 18px;
+  position: relative;
+  :focus-visible {
+    outline: none;
+  }
+  ::-webkit-input-placeholder {
+    /* Edge */
+    height: 24px;
+    line-height: 24px;
+    font-size: 18px;
+    color: #bebfc6;
+    position: absolute;
+    top: 30%;
+  }
+
+  :-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    height: 24px;
+    line-height: 24px;
+    font-size: 18px;
+    color: #bebfc6;
+    position: absolute;
+    top: 30%;
+  }
+
+  ::placeholder {
+    height: 24px;
+    line-height: 24px;
+    font-size: 18px;
+    color: #bebfc6;
+    position: absolute;
+    top: 30%;
+  }
+  div {
+    height: 24px;
+  }
+  @media (max-width: 600px) {
+    margin-top: 16px;
+    padding: 13px 15px;
+    width: 100%;
+    margin-right: 0px;
+  }
+  @media (min-width: 600px) {
+    margin-top: 16px;
+    padding: 13px 15px;
+    width: 100%;
+    margin-right: 0px;
+  }
+  @media (min-width: 992px) {
+    width: auto;
+    margin-right: 18px;
+    margin-top: 73px;
+  }
+  @media (min-width: 1280px) {
     height: 48px;
     width: 393px;
-    color: #BEBFC6;
+    color: #bebfc6;
     font-family: Calibre Regular;
     font-size: 18px;
     letter-spacing: 0;
     line-height: 24px;
     margin-top: 78px;
-    border: 1px solid #92939B;
+    border: 1px solid #92939b;
     border-radius: 3px;
     box-sizing: border-box;
     background: transparent;
     padding: 0px 15px;
     margin-right: 18px;
-    position: relative;
-    :focus-visible{
-        outline: none;
+    :focus-visible {
+      outline: none;
     }
-    ::-webkit-input-placeholder { /* Edge */
-        height: 24px;
-        line-height: 24px;
-        font-size: 18px;
-        color: #BEBFC6;
-        position: absolute;
-        top: 28%;
-      }
-      
-      :-ms-input-placeholder { /* Internet Explorer 10-11 */
-        height: 24px;
-        line-height: 24px;
-        font-size: 18px;
-        color: #BEBFC6;
-        position: absolute;
-        top: 28%;
-      }
-      
-      ::placeholder {
-        height: 24px;
-        line-height: 24px;
-        font-size: 18px;
-        color: #BEBFC6;
-        position: absolute;
-        top: 28%;
-      }
-      div{
-          height: 24px;
-      }
-      @media(max-width: 600px){
-        margin-top: 16px;
-        padding: 13px 15px;
-        width: 100%;
-        margin-right: 0px;
+    ::-webkit-input-placeholder {
+      /* Edge */
+      height: 24px;
+      line-height: 24px;
+      font-size: 18px;
+      color: #bebfc6;
     }
-    @media(min-width: 600px){
-        margin-top: 16px;
-        padding: 13px 15px;
-        width: 100%;
-        margin-right: 0px;
+
+    :-ms-input-placeholder {
+      /* Internet Explorer 10-11 */
+      height: 24px;
+      line-height: 24px;
+      font-size: 18px;
+      color: #bebfc6;
     }
-    @media(min-width: 992px){
-        width: auto;
-        margin-right: 18px;
-        margin-top: 73px;
+
+    ::placeholder {
+      height: 24px;
+      line-height: 26px;
+      font-size: 18px;
+      color: #bebfc6;
     }
-    @media(min-width: 1280px){
-        height: 48px;
-    width: 393px;
-    color: #BEBFC6;
-    font-family: Calibre Regular;
-    font-size: 18px;
-    letter-spacing: 0;
-    line-height: 24px;
-    margin-top: 78px;
-    border: 1px solid #92939B;
-    border-radius: 3px;
-    box-sizing: border-box;
-    background: transparent;
-    padding: 0px 15px;
-    margin-right: 18px;
-    :focus-visible{
-        outline: none;
-    }
-    ::-webkit-input-placeholder { /* Edge */
-        height: 24px;
-        line-height: 24px;
-        font-size: 18px;
-        color: #BEBFC6;
-      }
-      
-      :-ms-input-placeholder { /* Internet Explorer 10-11 */
-        height: 24px;
-        line-height: 24px;
-        font-size: 18px;
-        color: #BEBFC6;
-      }
-      
-      ::placeholder {
-        height: 24px;
-        line-height: 24px;
-        font-size: 18px;
-        color: #BEBFC6;
-      }
-    }
+  }
 `
 const Submit = styled.button`
+  background: transparent;
+  box-sizing: border-box;
+  height: 48px;
+  border: 2px solid #fecf09;
+  border-radius: 3px;
+  color: #ffffff;
+  font-family: "Calibre Semibold";
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: 0;
+  line-height: 22px;
+  text-align: center;
+  margin-top: 78px;
+  padding: 14px 45px;
+  :hover {
+    color: #101010;
+    background: #fecf09;
+    border: 2px solid #fecf09;
+  }
+  :focus {
+    outline: none;
+  }
+  @media (max-width: 600px) {
+    margin-top: 16px;
+    width: 100%;
+    padding: 13px 45px;
+  }
+  @media (min-width: 600px) {
+    margin-top: 16px;
+    width: 100%;
+    padding: 13px 45px;
+  }
+  @media (min-width: 992px) {
+    width: auto;
+    margin-top: 73px;
+  }
+  @media (min-width: 1280px) {
     background: transparent;
     box-sizing: border-box;
     height: 48px;
-    border: 2px solid #FECF09;
+    border: 2px solid #fecf09;
     border-radius: 3px;
-    color: #FFFFFF;
+    color: #ffffff;
     font-family: Calibre Semibold;
     font-size: 18px;
     font-weight: 600;
@@ -278,56 +339,18 @@ const Submit = styled.button`
     text-align: center;
     margin-top: 78px;
     padding: 14px 45px;
-    :hover{
-        color: #101010;
-        background: #fecf09;
-        border: 2px solid #FECF09;
-    }
-    :focus{
-        outline: none;
-    }
-    @media(max-width: 600px){
-        margin-top: 16px;
-        width: 100%;
-        padding: 13px 45px;
-    }
-    @media(min-width: 600px){
-        margin-top: 16px;
-        width: 100%;
-        padding: 13px 45px;
-    }
-    @media(min-width: 992px){
-        width: auto;
-        margin-top: 73px;
-    }
-    @media(min-width: 1280px){
-        background: transparent;
-        box-sizing: border-box;
-        height: 48px;
-        border: 2px solid #FECF09;
-        border-radius: 3px;
-        color: #FFFFFF;
-        font-family: Calibre Semibold;
-        font-size: 18px;
-        font-weight: 600;
-        letter-spacing: 0;
-        line-height: 22px;
-        text-align: center;
-        margin-top: 78px;
-        padding: 14px 45px;
-    }
+  }
 `
 const DivInput = styled.div`
-    display: block;
+  display: block;
 `
 const Errors = styled.p`
-    color: #F10909;;
-    padding-top: ${props => props.hienthi === '' ? "0px" : '10px'};
-    text-align : center;
-    display: ${props => props.hienthi === '' ? "none" : 'block'};
-    margin-bottom: 0px;
-    line-height: 24px;
-    font-size: 18px;
-    font-family: Calibre Regular;
-
+  color: #f10909;
+  padding-top: ${props => (props.hienthi === "" ? "0px" : "10px")};
+  text-align: center;
+  display: ${props => (props.hienthi === "" ? "none" : "block")};
+  margin-bottom: 0px;
+  line-height: 24px;
+  font-size: 18px;
+  font-family: "Calibre Regular";
 `
