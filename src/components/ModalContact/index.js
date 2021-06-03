7290eaf30294c37fead144afc9d5257706122590
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-06-03 18:51:34 +0700

    fix bugs

--- src/components/ModalContact/index.js
+++ src/components/ModalContact/index.js
@@ -242,12 +242,11 @@ const Input = styled.input`
   }
   @media (max-width: 320px) {
     margin-bottom: 20px;
-    padding-top: 11px;
   }
   @media (max-width: 600px) {
     border: 1px solid #6e6e6e;
     margin-bottom: 20px;
-    padding-top: 8px;
+    padding-top: 10px;
     height: 48px;
     padding-left: 14px;
 
