3f12f299fdeedebaff2e114f72c7b6d44fe5c9ee
Hiep Trinh Van <hieptv@arrowhitech.net>
2021-06-08 17:06:36 +0700

    make done page teamplate

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,8 +6,6 @@ module.exports.createPages = async ({ graphql, actions }) => {
   const BlogDetails = path.resolve("./src/templates/BlogDetails.js")
   const PageTemplate = path.resolve("./src/templates/page/index.js")
 
-  // const { GET_PAGES } = require("./src/queries/GetPage")
-
   const res = await graphql(`
     query IndexQuery {
       prismic {
@@ -44,61 +42,61 @@ module.exports.createPages = async ({ graphql, actions }) => {
   `)
 
   const pagesResult = await graphql(`
-    query GET_PAGE {
-      prismic {
-        allPages {
-          edges {
-            node {
-              page_name
-              _meta {
-                uid
-              }
-              body {
-                ... on PRISMIC_PageBody2_column_text {
-                  type
-                  label
-                  primary {
-                    text_side_left
-                    text_side_right
-                  }
-                }
-                ... on PRISMIC_PageBody3_column_profiles {
-                  type
-                  label
-                  fields {
-                    position
-                    name
-                    description
-                    avatar
-                  }
-                }
-                ... on PRISMIC_PageBodyBig_image {
-                  type
-                  fields {
-                    content_large
-                  }
-                  label
-                  primary {
-                    big_image
-                  }
-                }
-                ... on PRISMIC_PageBodyTitle_with_description {
-                  type
-                  label
-                  primary {
-                    text_description
-                    title
-                  }
-                }
-              }
-              meta_title
-              meta_description
-              keywords
+  query GET_PAGE {
+    prismic {
+      allPages {
+        edges {
+          node {
+            page_name
+            _meta {
+              uid
             }
+            body {
+              ... on PRISMIC_PageBody2_column_text {
+                type
+                label
+                primary {
+                  text_side_left
+                  text_side_right
+                }
+              }
+              ... on PRISMIC_PageBody3_column_profiles {
+                type
+                label
+                fields {
+                  position
+                  name
+                  description
+                  avatar
+                }
+              }
+              ... on PRISMIC_PageBodyBig_image {
+                type
+                fields {
+                  content_large
+                }
+                label
+                primary {
+                  big_image
+                }
+              }
+              ... on PRISMIC_PageBodyTitle_with_description {
+                type
+                label
+                primary {
+                  text_description
+                  title
+                }
+              }
+            }
+            meta_title
+            meta_description
+            keywords
           }
         }
       }
     }
+  }
   `)
 
   console.log("dataa pages  :", pagesResult)
